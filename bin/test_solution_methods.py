bd7fb729c2020679106767db6b53501044533d3b
Jeremy Stober <stober@gmail.com>
2012-07-19 12:46:53 -0500

    Testing model estimation + value iteration.

--- bin/test_solution_methods.py
+++ bin/test_solution_methods.py
@@ -7,41 +7,44 @@ Description: Try different solution methods for gridworld problems.
 """
 
 from gridworld.gridworldgui import GridworldGui
+import pdb
 from lspi import LSPI
 import numpy as np
-from td import TDQ,TD,Sarsa
+from td import TDQ,TD,Sarsa,SampleModelValueIteration
 import time
 
 endstates = [40]
 gw = GridworldGui(nrows=9,ncols=9,endstates=endstates, walls=[])
+#gw.updategui=False
 #gw.draw_state_labels()
 
 
 #learner = TDQ(8,81,0.1,0.9,0.9)
 #learner = TD(81,0.1,0.9,0.9)
-learner = Sarsa(8,81, 0.3, 0.9,0.9, 0.1)
-#learner.learn(100,gw,verbose=True)
+#learner = Sarsa(8,81, 0.3, 0.9,0.9, 0.4)
+learner = SampleModelValueIteration(8,81)
 
-v,pi = gw.value_iteration()
+# rw_model, transition_model
+# pdb.set_trace()
+v,pi = learner.learn(100,gw,verbose=True)
+
+#v,pi = gw.value_iteration()
+#pi = np.ones(gw.nstates,dtype='int')
 
 #vals = { s : learner.value(s) for s in range(gw.nstates) }
 #print vals
 #gw.draw_values(vals)
 
 # for s in range(gw.nstates):
-#     #a = learner.best(s)
-#     a = pi[s]
-#     gw.draw_arrow(s,a)
+#     a = learner.best(s)
+#     pi[s] = learner.best(s)
 
 # gw.redraw()
 
-# pi = np.ones(gw.nstates,dtype='int')
 # for i in gw.sindices:
 #     pi[i] = i % 8
 gw.set_arrows(pi)
 
-def policy(state):
-    return pi[state]
 
 # for i in gw.sindices[11:]:
 #     gw.state2circle(i)
@@ -53,7 +56,8 @@ def policy(state):
 #         gw.current = i
 
 # s = gw.endstates[0]
-# gw.follow(s-1,policy)
+# gw.follow(s-1,pi__getitem__)
+
 for i in gw.sindices:
-    gw.follow(i,policy)
+    gw.follow(i,pi.__getitem__)
 

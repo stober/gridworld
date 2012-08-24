50d6a9a6452e5ecfb15b265787065814f406f7f1
Jeremy Stober <stober@gmail.com>
2012-08-24 14:36:50 -0500

    dok seems to work the best.

--- src/markovdp.py
+++ src/markovdp.py
@@ -13,6 +13,7 @@ import random as pr
 import numpy as np
 import numpy.random as npr
 import scipy.sparse as sp
+from utils import sp_create
 
 class Features( object ):
     """
@@ -28,13 +29,10 @@ class Features( object ):
     def nfeatures(self):
         return self.feature_cnt
 
-    def phi(self, s, a, sparse=False, format="csr"):
+    def phi(self, s, a, sparse=False, format="dok"):
         
         if sparse:
-            if format == "dok"
-                sparse_features = sp.dok_matrix((self.feature_cnt,1))
-            elif format == "csr":
-                sparse_features = sp.csr_matrix((self.feature_cnt,1))
+            sparse_features = sp_create(self.feature_cnt,1,format)
             sparse_features[s + (a * self.nstates),0] = 1.0
             sparse_features[-1,0] = 1.0
             return sparse_features
